956b8f2e7c35f3a9a8f074aee5a84b5686138553
Beno Dwianto <benodwianto9@gmail.com>
2023-09-06 00:48:54 +0700

    Menambah file authorcontroller, dan menambah author posts

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ * @extends \Illuminate\PDatabase\Eloquent\Factories\Factory<\App\Models\Post>
  */
 class PostFactory extends Factory
 {
@@ -17,7 +17,12 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence(mt_rand(2, 8)),
+            'slug' => $this->faker->slug(),
+            'excerpt' => $this->faker->sentence(mt_rand(2, 10)),
+            'body' => $this->faker->paragraph(mt_rand(5, 10)),
+            'user_id' => mt_rand(1, 5),
+            'category_id' => mt_rand(1, 3)
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\models\Category;
+use App\models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -54,3 +56,5 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'category' => $category->name
     ]);
 });
+
+Route::get('/authors/{user:username}', [AuthorController::class, 'show']);

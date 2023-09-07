86f30682a53de666d32a01ceb08a3f84a62169fe
Beno Dwianto <benodwianto9@gmail.com>
2023-09-07 22:45:07 +0700

    Mengubah tampilan view category

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -6,22 +6,17 @@
         <div class="row">
             @foreach($categories as $category)
             <div class="col-md-4">
-                <div class="card bg-dark text-white">
-                    <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="card-img" alt="...">
-                    <div class="card-img-overlay">
-                      <h5 class="card-title">{{ $category->name }}</h5>
+                <a href="/categories/{{ $category->slug }}">
+                    <div class="card bg-dark text-white">
+                        <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="card-img" alt="...">
+                        <div class="card-img-overlay d-flex align-items-center p-0">
+                        <h5 class="card-title text-center flex-fill p-4 font-size-3" style="background-color:rgba(0,0,0,0.7) ">{{ $category->name }}</h5>
+                        </div>
                     </div>
-                  </div>
+                </a>
             </div>
             @endforeach
         </div>
     </div>
-        @foreach ($categories as $category)
-                <ul>
-                    <li>
-                        <h2><a href="/categories/{{ $category->slug}}">{{ $category->name }}</a></h2>
-                    </li>
-                </ul>
-        @endforeach
     @endsection
  
